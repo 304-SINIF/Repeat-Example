42634ec69a68fb1e6b1fceb947c2cf49d30557b0
HuseyinAk <hu_akbulut@hotmail.com>
2022-09-30 17:34:51 +0300

    #2 eklendi

--- Repeat.Example/Program.cs
+++ Repeat.Example/Program.cs
@@ -34,6 +34,78 @@ namespace Repeat.Example
             //Console.WriteLine(result);
 
             #endregion
+
+            #region Conditions (3.0)
+
+            // START 3.1
+            //int x = 5;
+            //int y = 3;
+            //if (x > 6)
+            //{
+            //    Console.WriteLine("X altıdan büyüktür");
+            //}
+            //else if (x * y != 99)
+            //{
+            //    Console.WriteLine("Formül geçersiz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hiçbir koşula uyum sağlanmadı");
+            //}
+            // END 3.1
+
+            // START 3.2
+            //int x = 5;
+            //switch(x)
+            //{
+            //    case 1:
+            //        Console.WriteLine("X = 1");
+            //        break;
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //        Console.WriteLine("X 2, 3 ya da 4 değerinderir");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("X = 5");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("X switch ağacındaki hiçbir değerle eşleşmemektedir");
+            //        break;
+            //}
+            // END 3.2
+            #endregion
+
+            #region Conversion (4.0)
+            // 4.1
+            //int i = 5;
+            //double d = Convert.ToDouble(i);
+
+            // 4.2
+            //string input = Console.ReadLine();
+            //double d = double.Parse(input);
+
+            // 4.3
+            //bool isParsable = int.TryParse("a5", out int x);
+
+            // 4.4
+            //    // 4.4.1
+            //int x = 5;
+            //string s = x.ToString();
+            //Console.WriteLine(x);
+
+            //    // 4.4.2
+            //Random r = new Random();
+            //Console.WriteLine(r.ToString());
+
+            //    // 4.4.3
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("hello, world!");
+            //Console.WriteLine(sb.ToString());
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
--- Repeat.Example/readme.txt
+++ Repeat.Example/readme.txt
@@ -24,3 +24,36 @@ Tanımlama formülleri
 		|| : (OR) İki önermeden birinin ya da her ikisinin de doğru olduğu durumlarda "true" döndürür
 		^  : (XOR) Önermelerin birbirinden farklı olduğu durumlarda "true" döndürür
 		<, <=, >, >=
+
+3.0
+	Program içerisinde mantıksal bir karar vermek için kullanılırlar
+	3.1
+		if
+		- Yukarıdan aşağıya, gerçekleşen koşulu bulana dek, if'in parantezleri arasındaki kodu çalıştırarak bir boolean sonuç bekler
+		- Sadece if, if-else if(∞), if-else, if-else if(∞)-else, şeklinde karar ağacı kurabilirsiniz
+	3.2
+		switch
+		- Yalnızca eşitlik kontrolü yapılabilir
+		- case ifadeleri alt alta yazıldığında OR anlamına gelir
+		
+4.0
+	Bir tipin bir başka tipe dönüştürülmesi için kullanılır. Örneğin konsoldan yapılan giriş tipi string olarak geleceğinden toplama işlemi yaptığımızda bu ifade concate edilir (stringlerde toplama), fakat amaç iki sayının aritmetik toplamıysa, konsoldan gelen değerin numerik bir değere dönüştürülmesi gerekir
+
+		4.1
+		Convert classı içerisinde tipler birbirine dönüşebilecekleri methodlar vardır. Örneğin int'ten double'a çevrilebilir
+
+		4.2
+		Tiplerin altında barınan Parse methodu yalnızca stringden çeviri yapabilir
+
+		4.3
+		TryParse methodu, çevrilmek istenilen değerin çevrilip çevrilemeyeceğine bakar. Eğer çevrilebiliyorsa geriye "true" döndürür, değişen değeri "out" parametresinin değişkenine atar aksi takdirde "false" döndürür ve "out" değişkenin tipinin varsayılan değerini atar. Böylelikle "Exception Handling" yapmak yerine "Validation" yapılabilir
+
+		4.4
+		Her tip içerisinde ToString methodunu barındırır.
+			4.4.1
+				Bu method ilkel değişkenlerde, değerlerinin string versiyonunu döndürürken,
+			
+			4.4.2
+				geriye kalan tipler yalnızca tipinin ne olduğunu string bir ifade olarak döndürür
+			4.4.3
+				(eğer ne yapacağını belirtmiş olan bir tip değilse)
